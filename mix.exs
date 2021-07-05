52fbb4324efc300d30ae4a365990bce13a5242d5
Hugo <hugolacerdar@gmail.com>
2021-07-05 17:43:11 -0300

    Add persistence dep

--- mix.exs
+++ mix.exs
@@ -29,9 +29,8 @@ defmodule Mastery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.10", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:mastery_persistence, path: "mastery_persistence"}
     ]
   end
 end
